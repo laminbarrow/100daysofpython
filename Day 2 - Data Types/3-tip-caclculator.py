2ad4878c5d75ea3f99b135b765742be33208f336
Lamin Barrow <laminbarrow@gmail.com>
2023-03-26 19:04:46 +0000

    Added $ symbol

--- Day 2 - Data Types/3-tip-caclculator.py
+++ Day 2 - Data Types/3-tip-caclculator.py
@@ -11,7 +11,7 @@
 print("Welcome to the tip calculator")
 the_bill = float(input("What was the total bill? "))
 percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-people_count = int(input("How many people to split the bill? "))
+people_count = int(input("How many people to split the bill? $"))
 
 tip_to_pay = (percentage_tip / 100) * the_bill;
 total_bill = the_bill + tip_to_pay
