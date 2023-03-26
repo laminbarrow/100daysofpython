62335ec9153282285e5a8fc68a51000f8921182d
Lamin Barrow <laminbarrow@gmail.com>
2023-03-26 15:01:26 +0000

    Updated formula and added comments

--- Day 2 - Data Types/bmi.py
+++ Day 2 - Data Types/bmi.py
@@ -5,6 +5,7 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-height_float = float(height)
-bmi = float(weight) / (height_float * height_float)
+
+#BMI Explained - https://en.wikipedia.org/wiki/Body_mass_index
+bmi = float(weight) / float(height) ** 2
 print(int(bmi))
