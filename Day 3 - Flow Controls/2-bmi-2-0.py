7fbf61dbff0bc232cb5cd2fd0c892d5a63df47d7
Lamin Barrow <laminbarrow@gmail.com>
2023-03-27 17:48:34 +0000

    Body mass index 2.0

---
+++ Day 3 - Flow Controls/2-bmi-2-0.py
@@ -0,0 +1,48 @@
+# Instructions
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+# It should tell them the interpretation of their BMI based on the BMI value.
+
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+
+# Warning you should round the result to the nearest whole number. The interpretation message needs to include the words in bold from the interpretations above. e.g. underweight, normal weight, overweight, obese, clinically obese.
+
+# Example Input
+# weight = 85
+# height = 1.75
+# Example Output
+# 85 ÷ (1.75 x 1.75) = 27.755102040816325
+# Your BMI is 28, you are slightly overweight.
+# The testing code will check for print output that is formatted like one of the lines below:
+
+# "Your BMI is 18, you are underweight."
+# "Your BMI is 22, you have a normal weight."
+# "Your BMI is 28, you are slightly overweight."
+# "Your BMI is 33, you are obese."
+# "Your BMI is 40, you are clinically obese."
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = round(weight / (height ** 2))
+message = f"Your BMI is {bmi}, you "
+
+if bmi <= 18.5:
+    print(message + "are underweight")
+elif bmi < 25:
+    print(message + "have a normal weight")
+elif bmi < 30:
+    print(message + "are slightly overweight")
+elif bmi < 35:
+    print(message + "are obese")
+else:
+    print(message + "are clinically obese")    
